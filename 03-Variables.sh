f498a2e9d3490dcef7a8f63cea6b905bd8b4ab87
bipendevmohanboda <bipendev@gmail.com>
2025-09-18 21:54:41 -0400

    Variables added

---
+++ 03-Variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+PERSON1=Ramesh
+PERSON2=Suresh
+
+echo "$PERSON1:: Hi $PERSON2, How are you?"
+echo "$PERSON2:: I am fine how are you?"
+echo "$PERSON1:: What are you learning recently?"
+echo "$PERSON2:: I am learning shell scripting $PERSON1."
