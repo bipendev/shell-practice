e0a4c59333eb730e312fcb4f2235dbca77e2843f
bipendevmohanboda <bipendev@gmail.com>
2025-09-22 18:11:56 -0400

    some message

--- 08-special-vars.sh
+++ 08-special-vars.sh
@@ -3,4 +3,8 @@
 echo " All Variables passed to the script: $@"
 echo " All Variables passed to the script: $*"
 echo " Current script name: $0"
+echo " Current directory: $PWD"
+echo " Who is running the script: $USER"
+echo " Home directory of the user: $HOME"
+echo " PID of the SCRIPT: $$"
 
