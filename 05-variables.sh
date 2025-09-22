13c678276797c24deadc8f9b3510092324d85462
bipendevmohanboda <bipendev@gmail.com>
2025-09-22 17:44:50 -0400

    some message

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo " Please enter your PIN NUMBER"
-read -s PIN_NUMBER # here PIN_NUMBER is variable
+read -s PIN_NUMBER # here PIN_NUMBER is variable and -s will not show what we are typing
 echo " PIN entered is $PIN_NUMBER"
