417305bb95e31b42f5b7ed15400a1b4b6e3d9abf
bipendevmohanboda <bipendev@gmail.com>
2025-09-23 13:22:59 -0400

    some message

---
+++ 15-colorsinstallation.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+USERID=$(id -u)
+
+
+
+
+if [ $USERID -ne 0 ]; then
+    echo  " ERROR:: Please run the script with root privilege"
+    exit 1 # other than  0 is failure
+fi
+
+VALIDATE (){ # functions receive inputs through args
+    if [ $1 -ne 0 ]; then
+        echo -e " Installing $2 ....$R FAILURE $N "
+        exit 1
+    else
+        echo -e " Installing $2 ....$G SUCCESS $N "
+    fi
+
+}
+
+
+dnf list installed mysql
+# Install if it is not found
+if [ $? -ne 0 ]; then
+    dnf install mysql -y
+    VALIDATE $? "MySQL"
+else
+    echo  -e "MySQL already exist ....$G SKIPPING $N "
+
+dnf list installed nginx
+# Install if it is not found
+if [ $? -ne 0 ]; then
+    dnf install nginx -y
+    VALIDATE $? "Nginx"
+else
+    echo -e "Nginx already exist ....$G SKIPPING $N "
+
+
+dnf list installed python
+# Install if it is not found
+if [ $? -ne 0 ]; then
+    dnf install python3 -y
+    VALIDATE $? "python3"
+else
+    echo -e "python3 already exist ....$G SKIPPING $N "
