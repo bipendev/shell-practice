d3f37fdec3f3c98cd9fd52819f600f973ad7dfa3
bipendevmohanboda <bipendev@gmail.com>
2025-09-23 13:04:16 -0400

    some message

---
+++ 13-installation.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+    echo " ERROR:: Please run the script with root privilege"
+    exit 1 # other than  0 is failure
+fi
+
+VALIDATE (){ # functions receive inputs through args
+    if [ $1 -ne 0 ]; then
+        echo " ERROR:: Installing $2 is failure "
+        exit 1
+    else
+        echo " Installing $2 is SUCCESS "
+    fi
+
+}
+
+dnf install mysql -y
+
+VALIDATE $? "MySQL"
+
+dnf install nginx -y
+
+VALIDATE $? "nginx"
+
+dnf install mongodb-mongosh -y
+
+VALIDATE $? "mongosh"
