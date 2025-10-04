5a39c6977554a5eb130bf184fd775a6ededb591c
bipendevmohanboda <bipendev@gmail.com>
2025-10-04 17:15:46 -0400

    Some message

--- 19-loops.sh
+++ 19-loops.sh
@@ -1,35 +1,78 @@
 #!/bin/bash
 
+# USERID=$(id -u)
+
+# LOG_FOLDER=/var/log/shellscript
+# SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+# LOF_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
+
+# mkdir -p $LOG_FOLDER
+
+# echo
+
+# R="\e[31m"
+# G="\e[32m"
+# Y="\e[33m"
+# N="\e[0m"
+
+# if [ $USERID -ne 0 ]; then
+#     echo " ERROR: Please run the script with root privilege"
+#     exit 1
+# fi
+
+# Validate(){
+# if [ $1 -ne 0 ]; then
+#     echo "Installing $2...$R FAILURE $N" | tee -a $LOG_FILE
+#     exit 1
+# else
+#     echo "Installing $@...$G SUCCESS $N" | tee -a $LOG_FILE
+# fi
+# }
+
+# for package in $@
+# do
+#     echo "package is: $package"
+# done
+
+
+#Variables
 USERID=$(id -u)
-
-LOG_FOLDER=/var/log/shellscript
-SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
-LOF_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
-
-mkdir -p $LOG_FOLDER
-
-echo
 
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-if [ $USERID -ne 0 ]; then
-    echo " ERROR: Please run the script with root privilege"
+#Condition
+if [ "$USERID" -ne 0 ]; then
+    echo " Error: Please run the script with root privileges"
     exit 1
 fi
 
-Validate(){
-if [ $1 -ne 0 ]; then
-    echo "Installing $2...$R FAILURE $N" | tee -a $LOG_FILE
-    exit 1
-else
-    echo "Installing $@...$G SUCCESS $N" | tee -a $LOG_FILE
+VALIDATE(){
+if [ "$1" -ne 0 ]; then
+        echo " Installing $2 ....$R FAILED $N"
+    
+    else
+        echo " Installing $2 .... $G SUCCESS $N"
+
 fi
+
 }
 
-for package in $@
-do
-    echo "package is: $package"
-done
+#Install package
+
+for package in  "$@"
+    do
+
+        dnf ilist installed "$package" 
+
+        if [ ! "$package" ]; then
+            dnf install "$package" -y
+            VALIDATE $? "$package" 
+        else
+              
+            echo " $package already installed...$Y SKIPPING $N"
+        fi
+    done
+
