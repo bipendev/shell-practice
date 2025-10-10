debba20784bb127cc48dda1b9a53a82e7f40457c
bipendevmohanboda <bipendev@gmail.com>
2025-10-10 19:35:55 -0400

    Some message

--- awsinstancecreate.sh
+++ awsinstancecreate.sh
@@ -1,2 +1,5 @@
+#!/bin/bash
+
+# Create instance
 INSTANCE=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-02236654aa239ac1b --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=ansible}]' --query 'Instances[0].InstanceId' --output text)
 echo "Launched instance ID: $INSTANCE"
--- awsinstancedelete.sh
+++ awsinstancedelete.sh
@@ -1,6 +1,35 @@
-RUNNING=$(aws ec2 describe-instances --filters "Name=instance-state-name,Values=running" --query "Reservations[*].Instances[*].InstanceId" --output text)
-echo "Instances Running: $RUNNING"
+# RUNNING=$(aws ec2 describe-instances --filters "Name=instance-state-name,Values=running" --query "Reservations[*].Instances[*].InstanceId" --output text)
+# echo "Instances Running: $RUNNING"
 
-DELETE=$(aws ec2 terminate-instances --instance-ids $(aws ec2 describe-instances --filters "Name=instance-state-name,Values=running" --query "Reservations[*].Instances[*].InstanceId" --output text))
-echo "Deleted Instances and current running instances are: $RUNNING"
+# DELETE=$(aws ec2 terminate-instances --instance-ids $(aws ec2 describe-instances --filters "Name=instance-state-name,Values=running" --query "Reservations[*].Instances[*].InstanceId" --output text))
+# echo "Deleted Instances and current running instances are: $RUNNING"
 
+#!/bin/bash
+
+# Get all running instances
+RUNNING=$(aws ec2 describe-instances \
+    --filters "Name=instance-state-name,Values=running" \
+    --query "Reservations[*].Instances[*].InstanceId" \
+    --output text)
+
+if [ -z "$RUNNING" ]; then
+    echo "No running instances found."
+    exit 0
+fi
+
+echo "Instances to be terminated: $RUNNING"
+
+# Terminate instances
+aws ec2 terminate-instances --instance-ids $RUNNING
+
+# Wait until instances are terminated
+echo "Waiting for instances to terminate..."
+aws ec2 wait instance-terminated --instance-ids $RUNNING
+
+# Verify
+TERMINATED=$(aws ec2 describe-instances \
+    --filters "Name=instance-state-name,Values=terminated" \
+    --query "Reservations[*].Instances[*].InstanceId" \
+    --output text)
+
+echo "Terminated instances: $TERMINATED"
