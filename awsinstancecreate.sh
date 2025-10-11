90f3880b0594dda3a6383fb829e6dbce0894fb8f
bipendevmohanboda <bipendev@gmail.com>
2025-10-10 20:04:20 -0400

    Some message

--- awsinstancecreate.sh
+++ awsinstancecreate.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+# #!/bin/bash
 
-# Create instance
-INSTANCE=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-02236654aa239ac1b --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=ansible}]' --query 'Instances[0].InstanceId' --output text)
-echo "Launched instance ID: $INSTANCE"
+# # Create instance
+# INSTANCE=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-02236654aa239ac1b --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=ansible}]' --query 'Instances[0].InstanceId' --output text)
+# echo "Launched instance ID: $INSTANCE"
 
 
 AMI_ID="ami-09c813fb71547fc4f"
